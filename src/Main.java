a1825b987f2ac779f51710b7c48de78c8205f1e2
Masha <Mariaalekseychuk@gmail.com>
2014-08-08 22:48:21 +0300

    add all libs and files

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
-/**
- * Created by Masha on 08.08.2014.
- */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 
 public class Main {
 
-    public static void main(String ars[]) {
 
-        System.out.println("Hello");
+    public static void main(String[] args) throws Exception {
 
+        DesiredCapabilities capability = DesiredCapabilities.firefox();
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+
+        // Go to the Google Suggest home page
+        driver.get("http://www.google.com/webhp?complete=1&hl=en");
+
+        driver.quit();
     }
 }
